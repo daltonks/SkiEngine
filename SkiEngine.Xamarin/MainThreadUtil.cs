1e132b6d039156c946c84461fe8d2b2506fb5971
Dalton Spillman <dalton_spillman@yahoo.com>
2019-05-21 20:10:02 -0500

    Created MainThreadUtil.cs. Updated Xamarin.Forms.

---
+++ SkiEngine.Xamarin/MainThreadUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SkiEngine.Xamarin
+{
+    public static class MainThreadUtil
+    {
+        public static Task InvokeOnMainThreadAsync(Func<Task> asyncFunc)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            Device.BeginInvokeOnMainThread(async () => {
+                try
+                {
+                    await asyncFunc.Invoke();
+                    tcs.TrySetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            });
+
+            return tcs.Task;
+        }
+    }
+}
