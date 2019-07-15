c64062b9520ee7c4db9271e3d2095f2bb0b4660a
Dalton Spillman <dalton_spillman@yahoo.com>
2019-07-14 21:17:34 -0500

    Color picker saturation/value seems to render nicely now

--- SkiEngine.Xamarin/ColorPicker/ColorPickerView.xaml.cs
+++ SkiEngine.Xamarin/ColorPicker/ColorPickerView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp.Views.Forms;
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using SkiEngine.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -6,26 +7,8 @@ using Xamarin.Forms.Xaml;
 namespace SkiEngine.Xamarin.ColorPicker
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ColorPickerView : SkiCanvasView
+    public partial class ColorPickerView : ContentView
     {
-        public static readonly BindableProperty SelectedColorProperty = BindableProperty.Create(
-            nameof(SelectedColor),
-            typeof(Color),
-            typeof(ColorPickerView),
-            Color.Black,
-            BindingMode.TwoWay,
-            propertyChanged: (bindable, oldValue, newValue) =>
-            {
-                ((ColorPickerView) bindable).InvalidateSurface();
-            }
-        );
-
-        public Color SelectedColor
-        {
-            get => (Color) GetValue(SelectedColorProperty);
-            set => SetValue(SelectedColorProperty, value);
-        }
-
         public ColorPickerView()
         {
             InitializeComponent();
@@ -33,10 +16,47 @@ namespace SkiEngine.Xamarin.ColorPicker
 
         private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
+            var info = e.Info;
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            // Hue
+            canvas.Clear(SKColors.Red);
+
+            using (var saturationPaint = new SKPaint())
+            {
+                using (
+                    saturationPaint.Shader = SKShader.CreateLinearGradient(
+                        new SKPoint(0, 0),
+                        new SKPoint(info.Width, 0),
+                        new[] {SKColors.White, SKColors.Transparent},
+                        new float[] {0, 1},
+                        SKShaderTileMode.Clamp
+                    )
+                )
+                {
+                    canvas.DrawRect(0, 0, info.Width, info.Height, saturationPaint);
+                }
+            }
             
+            using (var valuePaint = new SKPaint { BlendMode = SKBlendMode.SrcOver })
+            {
+                using (
+                    valuePaint.Shader = SKShader.CreateLinearGradient(
+                        new SKPoint(0, 0),
+                        new SKPoint(0, info.Height),
+                        new[] {SKColors.Empty, SKColors.Black},
+                        new float[] {0, 1},
+                        SKShaderTileMode.Clamp
+                    )
+                )
+                {
+                    canvas.DrawRect(0, 0, info.Width, info.Height, valuePaint);
+                }
+            }
         }
 
-        private void OnTouch(SkiTouch touch)
+        private void OnTouch(SkiTouch obj)
         {
             
         }
