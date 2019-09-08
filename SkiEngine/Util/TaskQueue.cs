f3e2b33b63e446819ea9eb52f430f88bc566af2f
Dalton Spillman <dalton_spillman@yahoo.com>
2019-09-07 19:29:36 -0500

    TaskQueue.NumTasksQueued

--- SkiEngine/Util/TaskQueue.cs
+++ SkiEngine/Util/TaskQueue.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SkiEngine.Util
 {
-    public class TaskQueue
+    public class TaskQueue : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private readonly object _locker = new object();
         private volatile Task _lastTask = Task.CompletedTask;
         private volatile bool _isShutdown;
+
+        private volatile int _numTasksQueued;
+        public int NumTasksQueued => _numTasksQueued;
 
         public Task QueueAsync(Action action)
         {
@@ -19,13 +27,21 @@ namespace SkiEngine.Util
 
         public Task<T> QueueAsync<T>(Func<T> function)
         {
+            Interlocked.Increment(ref _numTasksQueued);
+            OnPropertyChanged(nameof(NumTasksQueued));
+
             lock (_locker)
             {
                 var resultTask = _lastTask.ContinueWith(
                     _ =>
                     {
                         ThrowIfShutdown();
-                        return function();
+                        var result = function();
+
+                        Interlocked.Decrement(ref _numTasksQueued);
+                        OnPropertyChanged(nameof(NumTasksQueued));
+
+                        return result;
                     }, 
                     TaskContinuationOptions.RunContinuationsAsynchronously
                 );
@@ -36,13 +52,21 @@ namespace SkiEngine.Util
 
         public Task QueueAsync(Func<Task> asyncAction)
         {
+            Interlocked.Increment(ref _numTasksQueued);
+            OnPropertyChanged(nameof(NumTasksQueued));
+
             lock (_locker)
             {
                 _lastTask = _lastTask.ContinueWith(
                     _ =>
                     {
                         ThrowIfShutdown();
-                        return asyncAction();
+                        var result = asyncAction();
+
+                        Interlocked.Decrement(ref _numTasksQueued);
+                        OnPropertyChanged(nameof(NumTasksQueued));
+
+                        return result;
                     }, 
                     TaskContinuationOptions.RunContinuationsAsynchronously
                 ).Unwrap();
@@ -52,13 +76,22 @@ namespace SkiEngine.Util
 
         public Task<T> QueueAsync<T>(Func<Task<T>> asyncFunction)
         {
+            Interlocked.Increment(ref _numTasksQueued);
+            OnPropertyChanged(nameof(NumTasksQueued));
+
             lock (_locker)
             {
                 var resultTask = _lastTask.ContinueWith(
                     _ =>
                     {
                         ThrowIfShutdown();
-                        return asyncFunction();
+
+                        var result = asyncFunction();
+
+                        Interlocked.Decrement(ref _numTasksQueued);
+                        OnPropertyChanged(nameof(NumTasksQueued));
+
+                        return result;
                     }, 
                     TaskContinuationOptions.RunContinuationsAsynchronously
                 ).Unwrap();
@@ -81,6 +114,11 @@ namespace SkiEngine.Util
             return QueueAsync(() => {
                 _isShutdown = true;
             });
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
