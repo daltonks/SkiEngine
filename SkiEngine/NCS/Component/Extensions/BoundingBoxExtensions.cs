b0a71c3f04de59b7008c81ee5e54282b6f93cb03
Dalton Spillman <dalton_spillman@yahoo.com>
2020-02-21 19:12:00 -0600

    ILocalBoundingBox and BoundingBoxContains extension method

---
+++ SkiEngine/NCS/Component/Extensions/BoundingBoxExtensions.cs
@@ -0,0 +1,15 @@
+﻿using SkiaSharp;
+using SkiEngine.NCS.Component.Base;
+using SkiEngine.Util;
+
+namespace SkiEngine.NCS.Component.Extensions
+{
+    public static class BoundingBoxExtensions
+    {
+        public static bool BoundingBoxContains<T>(this T component, SKPoint worldPoint) where T : IComponent, ILocalBoundingBox
+        {
+            var localPoint = component.Node.WorldToLocalMatrix.MapPoint(worldPoint);
+            return component.LocalBoundingBox.Contains(localPoint);
+        }
+    }
+}
---
+++ SkiEngine/Util/ILocalBoundingBox.cs
@@ -0,0 +1,9 @@
+﻿using SkiaSharp;
+
+namespace SkiEngine.Util
+{
+    public interface ILocalBoundingBox
+    {
+        ref SKRect LocalBoundingBox { get; }
+    }
+}
