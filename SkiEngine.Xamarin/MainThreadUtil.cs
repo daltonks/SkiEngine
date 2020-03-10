4b3cf84f813a40c196690b5118952cc0fe9a8e21
Dalton Spillman <dalton_spillman@yahoo.com>
2020-03-10 00:25:12 -0500

    Revert "Remove MainThreadUtil"
    This reverts commit 0eb7fc836f78463f2755a9a29013ce6c4d9813c7.

---
+++ SkiEngine.Xamarin/MainThreadUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SkiEngine.Xamarin
+{
+    public static class MainThreadUtil
+    {
+        public static Task InvokeOnMainThreadAsync(Action action)
+        {
+            return InvokeOnMainThreadAsync(() => {
+                action.Invoke();
+                return Task.CompletedTask;
+            });
+        }
+
+        public static Task InvokeOnMainThreadAsync(Func<Task> asyncFunc)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            Device.BeginInvokeOnMainThread(async () => {
+                try
+                {
+                    await asyncFunc.Invoke();
+                    tcs.TrySetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            });
+
+            return tcs.Task;
+        }
+    }
+}
