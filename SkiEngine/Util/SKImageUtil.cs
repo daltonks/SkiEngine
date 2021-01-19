0f6a3aee3387503fe6a4aeb1f8b26a862efceb07
Dalton Spillman <daltonks@users.noreply.github.com>
2021-01-18 18:10:52 -0600

    SKImageUtil for rotating images based on SKEncodedOrigin

---
+++ SkiEngine/Util/SKImageUtil.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using SkiaSharp;
+
+namespace SkiEngine.Util
+{
+    // ReSharper disable once InconsistentNaming
+    public static class SKImageUtil
+    {
+        // https://github.com/mono/SkiaSharp/issues/836#issuecomment-584895517
+        public static (SKImage SKImage, SKEncodedImageFormat EncodedImageFormat) FixImageOrientation(Stream stream)
+        {
+            using var inputStream = new SKManagedStream(stream);
+            using var codec = SKCodec.Create(inputStream);
+            var encodedImageFormat = codec.EncodedFormat;
+            using var original = SKBitmap.Decode(codec);
+            var useWidth = original.Width;
+            var useHeight = original.Height;
+            Action<SKCanvas> transform = canvas => { };
+            switch (codec.EncodedOrigin)
+            {
+                case SKEncodedOrigin.TopLeft:
+                    // No transform needs to be applied.
+                    // Return original image.
+                    return (SKImage.FromBitmap(original), encodedImageFormat);
+                case SKEncodedOrigin.TopRight:
+                    // flip along the x-axis
+                    transform = canvas => canvas.Scale(-1, 1, useWidth / 2, useHeight / 2);
+                    break;
+                case SKEncodedOrigin.BottomRight:
+                    transform = canvas => canvas.RotateDegrees(180, useWidth / 2, useHeight / 2);
+                    break;
+                case SKEncodedOrigin.BottomLeft:
+                    // flip along the y-axis
+                    transform = canvas => canvas.Scale(1, -1, useWidth / 2, useHeight / 2);
+                    break;
+                case SKEncodedOrigin.LeftTop:
+                    useWidth = original.Height;
+                    useHeight = original.Width;
+                    transform = canvas =>
+                    {
+                        // Rotate 90
+                        canvas.RotateDegrees(90, useWidth / 2, useHeight / 2);
+                        canvas.Scale(useHeight * 1.0f / useWidth, -useWidth * 1.0f / useHeight, useWidth / 2, useHeight / 2);
+                    };
+                    break;
+                case SKEncodedOrigin.RightTop:
+                    useWidth = original.Height;
+                    useHeight = original.Width;
+                    transform = canvas =>
+                    {
+                        // Rotate 90
+                        canvas.RotateDegrees(90, useWidth / 2, useHeight / 2);
+                        canvas.Scale(useHeight * 1.0f / useWidth, useWidth * 1.0f / useHeight, useWidth / 2, useHeight / 2);
+                    };
+                    break;
+                case SKEncodedOrigin.RightBottom:
+                    useWidth = original.Height;
+                    useHeight = original.Width;
+                    transform = canvas =>
+                    {
+                        // Rotate 90
+                        canvas.RotateDegrees(90, useWidth / 2, useHeight / 2);
+                        canvas.Scale(-useHeight * 1.0f / useWidth, useWidth * 1.0f / useHeight, useWidth / 2, useHeight / 2);
+                    };
+                    break;
+                case SKEncodedOrigin.LeftBottom:
+                    useWidth = original.Height;
+                    useHeight = original.Width;
+                    transform = canvas =>
+                    {
+                        // Rotate 90
+                        canvas.RotateDegrees(90, useWidth / 2, useHeight / 2);
+                        canvas.Scale(-useHeight * 1.0f / useWidth, -useWidth * 1.0f / useHeight, useWidth / 2, useHeight / 2);
+                    };
+                    break;
+                default:
+                    break;
+            }
+
+            var imageInfo = new SKImageInfo(useWidth, useHeight, original.ColorType, original.AlphaType, original.ColorSpace);
+            using var surface = SKSurface.Create(imageInfo);
+            
+            // Transform based on origin
+            transform.Invoke(surface.Canvas);
+
+            // Draw bitmap
+            using var bitmapPaint = new SKPaint
+            {
+                IsAntialias = true, 
+                FilterQuality = SKFilterQuality.High
+            };
+            surface.Canvas.DrawBitmap(original, imageInfo.Rect, bitmapPaint);
+
+            surface.Canvas.Flush();
+
+            // Return transformed snapshot
+            return (surface.Snapshot(), encodedImageFormat);
+        }
+    }
+}
