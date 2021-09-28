aac16c0a32e775f8085e5c927106377cdf1bbc53
Dalton Spillman <daltonks@users.noreply.github.com>
2021-09-27 23:21:25 -0500

    CurveUtil to convert quad to cubic control points

---
+++ SkiEngine/Util/CurveUtil.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp;
+using SkiEngine.Util.Extensions.SkiaSharp;
+
+namespace SkiEngine.Util
+{
+    public static class CurveUtil
+    {
+        private const double TwoThirds = 2.0 / 3.0;
+
+        public static (SKPoint p1, SKPoint p2) QuadToCubic(SKPoint start, SKPoint control, SKPoint end)
+        {
+            return (
+                start + (control - start).Multiply(TwoThirds), 
+                end + (control - end).Multiply(TwoThirds)
+            );
+        }
+    }
+}
